08a70aa5386bd4d49351e494c265177ecb1990ac
unknown <00rohansahkr@gmail.com>
2023-02-24 20:33:47 +0530

    update_day-4

--- Script/logIn.js
+++ Script/logIn.js
@@ -4,21 +4,62 @@ let userSignInBtn = document.getElementById("signin-Btn");
 
 userSignInBtn.addEventListener("click", loginUser);
 
-function loginUser() {
-    const obj = {
-      email: email.value,
-      password: password.value,
-    };
-
-    const loginReq = fetch(`https://gents-hub.onrender.com/users`, {
-      method: "POST",
-      headers: {
-        "Content-type": "application/json",
-      },
-      body: JSON.stringify(obj),
-    }).then((res)=>{
-        console.log(res);
-    }).catch((error)=>{
-        console.log(error);
-    })
+let dataArr = [];
+async function loginUser(e) {
+  e.preventDefault();
+  const obj = {
+    email: email.value,
+    password: password.value,
+  };
+  console.log(obj);
+  // const loginReq = fetch(`https://gents-hub.onrender.com/users`, {
+  //   method: "GET",
+  //   headers: {
+  //     "Content-type": "application/json",
+  //   },
+  //   body: JSON.stringify(obj),
+  // })
+  try {
+    let res = await fetch(`https://gents-hub.onrender.com/users`);
+    let data = await res.json();
+    //    console.log(data);
+    data.forEach((item) => {
+      let obj = {
+        email: item.email,
+        password: item.password,
+      };
+      dataArr.push(obj);
+    });
+    console.log(dataArr);
+    let load = false;
+    for (let i = 0; i < dataArr.length; i++) {
+        let count=0;
+      for (let el in dataArr[i]) {
+        for (let key in obj) {
+          if (obj[key] === dataArr[i][el]) {
+            count++;
+          }
+        }
+      }
+      if(count==2){
+        load = true;
+        break;
+      }
+    }
+    if (load) {
+      window.location.assign("#");
+      alert(" Signed In Successfully!");
+    } else {
+      alert("Wrong Credentials,login failed!");
+    }
+  } catch (error) {
+    console.log(error);
   }
+}
+
+function create() {
+  window.location.assign("signUp.html");
+}
+function admin() {
+  window.location.assign("#");
+}
--- Script/signUp.js
+++ Script/signUp.js
@@ -16,16 +16,30 @@ async function RegisterUser() {
       password: password.value,
       number: phone.value,
     };
-
-    let request=await fetch(`https://gents-hub.onrender.com/users`,{
-        method: 'POST',
-        headers:{
-            "Content-type":'application/json'   
+    if (
+      firstName.value == "" ||
+      lastName.value == "" ||
+      email.value == "" ||
+      password.value == "" ||
+      number.value == ""
+    ) {
+      alert("Please fill out all required fields ");
+    } else {
+      let request = await fetch(`https://gents-hub.onrender.com/users`, {
+        method: "POST",
+        headers: {
+          "Content-type": "application/json",
         },
-        body : JSON.stringify(obj)
-    })
-    console.log(request);
+        body: JSON.stringify(obj),
+      });
+      window.location.assign("logIn.html");
+      alert("Congratulations! Your Account Created Sucessfully! Now SignIn for Explore.");
+    }
   } catch (error) {
     console.log("error");
   }
+}
+
+function log(){
+    window.location.assign("logIn.html");
 }
